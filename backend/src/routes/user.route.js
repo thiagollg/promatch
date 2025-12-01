55780f037462c0e3d3d9caf06ac3dd22a8282999
thiagollg <thiagolaleggia@gmail.com>
2025-12-01 13:01:59 -0300

    final

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { protectRoute } from "../../middleware/auth.middleware.js";
-import { getMyProffessors, createConnection, getProfessorById, checkConnectionStatus, searchProfessors, deleteCurrentUser, getSimilarProfessors, getRecommendedProfessorsBySubjects} from "../controllers/user.controller.js";
+import { getMyProffessors, createConnection, getProfessorById, checkConnectionStatus, searchProfessors, deleteCurrentUser, getRecommendedProfessorsBySubjects} from "../controllers/user.controller.js";
 
 const router = express.Router();
 
@@ -15,6 +15,6 @@ router.get("/professors/search", searchProfessors);
 router.get("/professor/:id", getProfessorById);
 router.delete("/me", deleteCurrentUser);
 router.get("/professor/:id/connection-status", checkConnectionStatus);
-router.get("/professor/:id/similar", getSimilarProfessors);
+
 
 export default router;
